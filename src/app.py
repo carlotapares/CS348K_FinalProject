fd351cb5707655dd2f163d3e24dd905d654cfe2b
Carlota Parés Morlans <carlota.pamo@gmail.com>
2022-06-02 16:10:15 -0700

    new app

--- src/app.py
+++ src/app.py
@@ -1,134 +1,140 @@
-import streamlit as st
-import pandas as pd
+from flask import Flask, render_template, request, redirect, url_for
+from flask_cors import CORS
 import numpy as np
-import argparse
-import os
+import json
 
-from ui_utils import Condition, get_dataset_subset, check_assertions
+#from ui_utils import Condition, get_dataset_subset, check_assertions
 
-DATASET_PATH = './dataset/'
+app = Flask(__name__,static_folder='static',template_folder='templates')
+CORS(app)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', default=['./dataset/'],
-                    help="Add dataset path")
+images = ["test1.jpeg", "test2.jpeg", "test3.jpeg"]
+keypoints = np.random.randint(0,100,10).tolist()
+bbox = [200,200,200,200]
+data_ = {"images": [images,images,images,images],
+        "keypoints":[keypoints,keypoints,keypoints,keypoints],
+        "bbox":[bbox,bbox,bbox,bbox]}
 
-try:
-  args = parser.parse_args()
-  DATASET_PATH = args.dataset[0]
-except SystemExit as e:
-  os._exit(e.code)
+@app.route('/')
+def index():
+  return render_template('index.html')
 
+@app.route('/search', methods = ['POST'])
+def search():
+  content = request.get_json(silent=True)
+  print(content)
+  return json.dumps(data_)
 
-def initialize_session_state():
- ## TODO initialize any session state needed - example below
-#   if 'seen_image_indices_list' not in st.session_state:
-#       st.session_state['seen_image_indices_list'] = []
-    return
+# def initialize_session_state():
+#  ## TODO initialize any session state needed - example below
+# #   if 'seen_image_indices_list' not in st.session_state:
+# #       st.session_state['seen_image_indices_list'] = []
+#     return
 
-initialize_session_state()
+# initialize_session_state()
 
-def reset_session_state():
-    #TODO clear any session state on reset
-    return
+# def reset_session_state():
+#     #TODO clear any session state on reset
+#     return
 
-# 2 TABS layout
-st.markdown(
-    '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">',
-    unsafe_allow_html=True,
-)
-query_params = st.experimental_get_query_params()
-tabs = ["Explore Dataset", "Detect Errors Using Assertions"]
-if "tab" in query_params:
-    active_tab = query_params["tab"][0]
-else:
-    active_tab = "Explore Dataset"
+# # 2 TABS layout
+# st.markdown(
+#     '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">',
+#     unsafe_allow_html=True,
+# )
+# query_params = st.experimental_get_query_params()
+# tabs = ["Explore Dataset", "Detect Errors Using Assertions"]
+# if "tab" in query_params:
+#     active_tab = query_params["tab"][0]
+# else:
+#     active_tab = "Explore Dataset"
 
-if active_tab not in tabs:
-    st.experimental_set_query_params(tab="Explore Dataset")
-    active_tab = "Explore Dataset"
+# if active_tab not in tabs:
+#     st.experimental_set_query_params(tab="Explore Dataset")
+#     active_tab = "Explore Dataset"
 
-li_items = "".join(
-    f"""
-    <li class="nav-item">
-        <a class="nav-link{' active' if t==active_tab else ''}" target="_self" rel="noopener noreferrer" href="/?tab={t}">{t}</a>
-    </li>
-    """
-    for t in tabs
-)
-tabs_html = f"""
-    <ul class="nav nav-tabs">
-    {li_items}
-    </ul>
-"""
-st.markdown(tabs_html, unsafe_allow_html=True)
-st.markdown("<br><br>", unsafe_allow_html=True)
+# li_items = "".join(
+#     f"""
+#     <li class="nav-item">
+#         <a class="nav-link{' active' if t==active_tab else ''}" target="_self" rel="noopener noreferrer" href="/?tab={t}">{t}</a>
+#     </li>
+#     """
+#     for t in tabs
+# )
+# tabs_html = f"""
+#     <ul class="nav nav-tabs">
+#     {li_items}
+#     </ul>
+# """
+# st.markdown(tabs_html, unsafe_allow_html=True)
+# st.markdown("<br><br>", unsafe_allow_html=True)
 
-# Helper for displaying results underneath tabs
+# # Helper for displaying results underneath tabs
 
-if 'get_subset' not in st.session_state:
-    st.session_state['get_subset'] = '--'
+# if 'get_subset' not in st.session_state:
+#     st.session_state['get_subset'] = '--'
     
-if 'find_errors' not in st.session_state:
-    st.session_state['find_errors'] = '--'
+# if 'find_errors' not in st.session_state:
+#     st.session_state['find_errors'] = '--'
 
-def find_errors():
-  st.session_state.find_errors = ''
+# def find_errors():
+#   st.session_state.find_errors = ''
 
-def get_subset():
-  st.session_state.get_subset = ''
+# def get_subset():
+#   st.session_state.get_subset = ''
 
-# Content under Explore Dataset tab
-if active_tab == "Explore Dataset":
-  with st.sidebar:
-    # mutliselect for 
-    query_condition_list = st.multiselect('Search Filters', [c.name for c in Condition])
-    num_batches = st.number_input('Number of batches', min_value=1, max_value=7, value=5)
-    batch_size = st.number_input('Number of frames per batch', min_value=1, max_value=7, value=5)
-    search_button = st.button("Search", on_click=get_subset)
+# # Content under Explore Dataset tab
+# if active_tab == "Explore Dataset":
+#   with st.sidebar:
+#     # mutliselect for 
+#     query_condition_list = st.multiselect('Search Filters', [c.name for c in Condition])
+#     num_batches = st.number_input('Number of batches', min_value=1, max_value=7, value=5)
+#     batch_size = st.number_input('Number of frames per batch', min_value=1, max_value=7, value=5)
+#     search_button = st.button("Search", on_click=get_subset)
 
-# Content under asstertions tab
-elif active_tab == "Detect Errors Using Assertions":
-  with st.sidebar:
-    num_batches = st.number_input('Number of assertions', min_value=1, max_value=10, value=1)
-    search_button = st.button("Find errors", on_click=find_errors)
+# # Content under asstertions tab
+# elif active_tab == "Detect Errors Using Assertions":
+#   with st.sidebar:
+#     num_batches = st.number_input('Number of assertions', min_value=1, max_value=10, value=1)
+#     search_button = st.button("Find errors", on_click=find_errors)
 
-else:
-    st.error("Something has gone terribly wrong.")
+# else:
+#     st.error("Something has gone terribly wrong.")
 
-# Check if search button has been clicked
-if st.session_state.get_subset != '--':
-  if (query_condition_list):
-    ## LAYOUT - 2 cols
-    cols = st.columns(batch_size)
+# # Check if search button has been clicked
+# if st.session_state.get_subset != '--':
+#   if (query_condition_list):
+#     ## LAYOUT - 2 cols
+#     cols = st.columns(batch_size)
 
-    # Get data
-    res, _ = get_dataset_subset(DATASET_PATH + 'wimbledon_2019_womens_final_halep_williams__fduc5bZx3ss',
-      query_condition_list,num_batches,batch_size, True)
+#     # Get data
+#     res, _ = get_dataset_subset(DATASET_PATH + 'wimbledon_2019_womens_final_halep_williams__fduc5bZx3ss',
+#       query_condition_list,num_batches,batch_size, True)
     
-    # Plot data
-    for b in range(num_batches):
-      for i in range(batch_size):
-        cols[i].image(res[b].get_frame_at(i).get_data(), use_column_width=True)
+#     # Plot data
+#     for b in range(num_batches):
+#       for i in range(batch_size):
+#         cols[i].image(res[b].get_frame_at(i).get_data(), use_column_width=True)
 
 
-# Check if find errors button has been clicked
-if st.session_state.find_errors != '--':
-  res, dataset = get_dataset_subset(DATASET_PATH + 'wimbledon_2019_womens_final_halep_williams__fduc5bZx3ss',
-    ['fast', 'player_back'],5, 10, False)
+# # Check if find errors button has been clicked
+# if st.session_state.find_errors != '--':
+#   res, dataset = get_dataset_subset(DATASET_PATH + 'wimbledon_2019_womens_final_halep_williams__fduc5bZx3ss',
+#     ['fast', 'player_back'],5, 10, False)
 
-  assertions = [{'keypoints': ['right_elbow','right_shoulder','right_wrist', 'right_shoulder'], 'type': 'spatial', 'attributes': ['above', 'above']}, \
-              {'keypoints': ['left_shoulder','right_shoulder','left_knee', 'right_knee', 'left_hip', 'right_hip'], 'type': 'spatial', 'attributes': ['left', 'left', 'left']}, \
-              {'keypoints': ['left_shoulder','right_shoulder','left_knee', 'right_knee', 'left_hip', 'right_hip'], 'type': 'spatial', 'attributes': ['right', 'right', 'right']}, \
-              {'keypoints': ['left_elbow','right_elbow','left_wrist','right_wrist'], 'type': 'spatial', 'attributes': [('smaller', 0.05),('smaller', 0.15)]}, \
-              {'keypoints': ['left_ankle','right_ankle','left_knee','right_knee'], 'type': 'spatial', 'attributes': [('smaller', 0.05),('smaller', 0.15)]}, \
-              {'keypoints': ['head_top','head_bottom'], 'type': 'spatial', 'attributes': [('smaller', 0.25)]}, \
-              {'keypoints': ['right_wrist'], 'type': 'temporal', 'attributes': [0.3]},
-              {'keypoints': ['left_wrist'], 'type': 'temporal', 'attributes': [0.3]}]
+#   assertions = [{'keypoints': ['right_elbow','right_shoulder','right_wrist', 'right_shoulder'], 'type': 'spatial', 'attributes': ['above', 'above']}, \
+#               {'keypoints': ['left_shoulder','right_shoulder','left_knee', 'right_knee', 'left_hip', 'right_hip'], 'type': 'spatial', 'attributes': ['left', 'left', 'left']}, \
+#               {'keypoints': ['left_shoulder','right_shoulder','left_knee', 'right_knee', 'left_hip', 'right_hip'], 'type': 'spatial', 'attributes': ['right', 'right', 'right']}, \
+#               {'keypoints': ['left_elbow','right_elbow','left_wrist','right_wrist'], 'type': 'spatial', 'attributes': [('smaller', 0.05),('smaller', 0.15)]}, \
+#               {'keypoints': ['left_ankle','right_ankle','left_knee','right_knee'], 'type': 'spatial', 'attributes': [('smaller', 0.05),('smaller', 0.15)]}, \
+#               {'keypoints': ['head_top','head_bottom'], 'type': 'spatial', 'attributes': [('smaller', 0.25)]}, \
+#               {'keypoints': ['right_wrist'], 'type': 'temporal', 'attributes': [0.3]},
+#               {'keypoints': ['left_wrist'], 'type': 'temporal', 'attributes': [0.3]}]
 
-  errors, frames = check_assertions(DATASET_PATH + 'wimbledon_2019_womens_final_halep_williams__fduc5bZx3ss', dataset, res, assertions, True)
+#   errors, frames = check_assertions(DATASET_PATH + 'wimbledon_2019_womens_final_halep_williams__fduc5bZx3ss', dataset, res, assertions, True)
 
-  if errors is not None:
-    st.dataframe(errors)
-    if frames is not None:
-      for f in frames:
-        st.image(f.get_data())
+#   if errors is not None:
+#     st.dataframe(errors)
+#     if frames is not None:
+#       for f in frames:
+#         st.image(f.get_data())
---
+++ src/static/script.js
@@ -0,0 +1,107 @@
+function addImage(canvasId, path, keypoints, bbox){
+  var canvas = document.getElementById(canvasId);
+
+  canvas.style["display"] = "inline";
+  canvas.style["background"] = `url(static/${path})`;
+  canvas.style["background-position"] = "-" + bbox[0].toString() + "px -" + bbox[1].toString() + "px";
+  canvas.style["width"] = bbox[2].toString() + 'px';
+  canvas.style["height"] = bbox[3].toString() + 'px';
+  canvas.style["background-repeat"] = 'no-repeat';
+
+  var ctx = canvas.getContext("2d");
+
+  ctx.moveTo(0, 0);
+  ctx.lineTo(200, 100);
+  ctx.stroke();
+
+  ctx.moveTo(100, 0);
+  ctx.lineTo(200, 100);
+  ctx.stroke();
+}
+
+function addFrameGroup(frame_id, path, keypoints){
+  id1 = frame_id.toString() + "1";
+  id2 = frame_id.toString() + "2";
+  id3 = frame_id.toString() + "3";
+  carousel_id = "carousel" + frame_id.toString();
+  carousel_href = "#" + carousel_id;
+
+  const exists = document.getElementById(id1);
+  if (exists != null) return;
+
+  const base_html = `
+  <div class="col">
+      <div class="card mb-3" style="border: 0px; width=auto !important">
+          <div id=${carousel_id} class="carousel" data-ride="carousel" data-interval="false">
+              <div class="carousel-inner">
+                  <div class="carousel-item">
+                      <canvas id=${id1} style="display: none;"></canvas>
+                  </div>
+                  <div class="carousel-item active">
+                      <canvas id=${id2} style="display: none;"></canvas>
+                  </div>
+                  <div class="carousel-item">
+                      <canvas id=${id3} style="display: none;"></canvas>
+                  </div>
+              </div>
+              <a class="carousel-control-prev" href=${carousel_href} role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              </a>
+              <a class="carousel-control-next" href=${carousel_href} role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              </a>
+          </div>
+      </div>
+  </div>
+  `;
+  document.getElementById('imageArea').innerHTML +=base_html;
+}
+
+function loadData(images, keypoints, bbox){
+  for (let i = 0; i < images.length; i++) {
+    addFrameGroup("frame" + i.toString(), images[i],keypoints[i]);
+  }
+  for (let i = 0; i < images.length; i++) {
+    addImage("frame" + i.toString() + "1", images[i][0], keypoints[i][0], bbox[i]);
+    addImage("frame" + i.toString() + "2", images[i][1], keypoints[i][1], bbox[i]);
+    addImage("frame" + i.toString() + "3", images[i][2], keypoints[i][2], bbox[i]);
+  }
+}
+
+function decode(html) {
+  var txt = document.createElement("textarea");
+  txt.innerHTML = html;
+  return txt.value;
+}
+
+function sendSelectionToServer(checkbox, batches, frames){
+var httpPost = new XMLHttpRequest(),
+path = "http://localhost:5000/search",
+data = JSON.stringify({checkbox: checkbox, batches: batches, frames: frames});
+httpPost.responseType = 'json';
+httpPost.open("POST", path, true);
+httpPost.setRequestHeader('Content-Type', 'application/json');
+httpPost.send(data);
+
+httpPost.onreadystatechange = function(err) {
+    if (httpPost.readyState == 4 && httpPost.status == 200){
+        const data = httpPost.response;
+        loadData(data['images'],data['keypoints'], data['bbox']);
+    } else {
+        console.log(err);
+    }
+  };
+};
+
+function searchClicked(){
+  var group = document.getElementsByClassName("form-check-input");
+  var checkbox = []
+  for (let i = 0; i < group.length; i++){
+    if (group[i].checked == true){
+      checkbox.push(group[i].value);
+    }
+  }
+  var batches = document.getElementById('batchRange').value;
+  var frames = document.getElementById('frameRange').value;
+  sendSelectionToServer(checkbox, batches, frames);
+}
